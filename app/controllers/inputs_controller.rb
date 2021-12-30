e08442e2fb31e7579c261dd4988d32ce2cdfc8a0
fujiyum <91727339+fujiyum@users.noreply.github.com>
2021-12-30 13:58:28 +0000

    [Update]note_controller

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -19,9 +19,15 @@ class InputsController < ApplicationController
   end
 
   def show
+    @title = Title.find(params[:title_id])
+    @user = @title.user
+    @input = @title.input.find(params[:id])
   end
 
   def edit
+    @title = Title.find(params[:title_id])
+    @user = @title.user
+    @input = Input.find(params[:id])
   end
 
   def update
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,19 +1,35 @@
 class NotesController < ApplicationController
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+    @note.save
+    redirect_to notes_path
   end
 
   def index
+    @title = Title.find(params[:title_id])
+    @notes = @title.note
   end
 
   def edit
+    @note = Note.find(params[:id])
   end
 
   def update
+    note = Note.find(params[:id])
+    note.update
   end
 
   def destroy
   end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title_id, :note)
+  end
+
 end
