6b5c4576cfd4d91287eb1f366ff7396cd7c93e42
fujiyum <91727339+fujiyum@users.noreply.github.com>
2022-01-04 13:33:20 +0000

    [Add]header&footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def after_sign_in_path_for(resources)
+        if current_user
+            flash[:notice] = "Welcome Back"
+            user_path(current_user.id)
+        else
+            flash[:notice] = "Welcome"
+            titles_path
+        end
+    end
+
     protected
 
     def configure_permitted_parameters
