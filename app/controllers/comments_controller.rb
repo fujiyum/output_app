923b8b2050a8c578e0d94879aac250cc7a911aa5
fujiyum <91727339+fujiyum@users.noreply.github.com>
2022-01-05 12:35:30 +0000

    [Update]comment_destroy機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,16 @@
 class CommentsController < ApplicationController
 
     def create
-    input = Input.find(params[:input_id])
-    comment = current_user.comments.new(comment_params)
-    comment.input_id = input.id
-    comment.save
-    redirect_to input_path(input)
+        input = Input.find(params[:input_id])
+        comment = current_user.comments.new(comment_params)
+        comment.input_id = input.id
+        comment.save
+        redirect_to input_path(input)
     end
 
     def destroy
+        Comment.find_by(id: params[:id]).destroy
+        redirect_to input_path(params[:input_id])
     end
 
     private
