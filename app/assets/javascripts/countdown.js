4e6fbda89d4a3ff57d3909f25b9eef9948225539
fujiyum <91727339+fujiyum@users.noreply.github.com>
2022-01-18 14:09:10 +0000

    [Add]countdown.js

---
+++ app/assets/javascripts/countdown.js
@@ -0,0 +1,50 @@
+function showCountdown() {
+   // 現在日時を数値(1970-01-01 00:00:00からのミリ秒)に変換
+   var nowDate = new Date();
+   var dnumNow = nowDate.getTime();
+
+   // 指定日時を数値(1970-01-01 00:00:00からのミリ秒)に変換
+   var targetDate = new Date("2022/1/30");
+   var dnumTarget = targetDate.getTime();
+
+   // 表示を準備
+   var dlYear  = targetDate.getFullYear();
+   var dlMonth = targetDate.getMonth() + 1;
+   var dlDate  = targetDate.getDate();
+   var dlHour  = targetDate.getHours();
+   var dlMin   = targetDate.getMinutes();
+   var dlSec   = targetDate.getSeconds();
+   var msg1 = "期限の" + dlYear + "/" + dlMonth + "/" + dlDate + " " + dlHour + ":" + dlMin + ":" + dlSec;
+
+   // 引き算して日数(ミリ秒)の差を計算
+   var diff2Dates = dnumTarget - dnumNow;
+
+   // 差のミリ秒を、日数・時間・分・秒に分割
+   var dDays  = diff2Dates / ( 1000 * 60 * 60 * 24 );   // 日数
+   diff2Dates = diff2Dates % ( 1000 * 60 * 60 * 24 );
+   var dHour  = diff2Dates / ( 1000 * 60 * 60 );   // 時間
+   diff2Dates = diff2Dates % ( 1000 * 60 * 60 );
+   var dMin   = diff2Dates / ( 1000 * 60 );   // 分
+   diff2Dates = diff2Dates % ( 1000 * 60 );
+   var dSec   = diff2Dates / 1000;   // 秒
+   var msg2 = Math.floor(dDays) + "日"
+            + Math.floor(dHour) + "時間"
+            + Math.floor(dMin) + "分"
+            + Math.floor(dSec) + "秒";
+
+   // 表示文字列の作成
+   var msg;
+   if( dnumTarget > dnumNow ) {
+      // まだ期限が来ていない場合
+      msg = msg1 + "までは、あと" + msg2 + "です。";
+   }
+   else {
+      // 期限が過ぎた場合
+      "Restart";
+   }
+
+   // 作成した文字列を表示
+   document.getElementById("RealtimeCountdownArea").innerHTML = msg;
+}
+// 1秒ごとに実行
+setInterval('showCountdown()',1000);
