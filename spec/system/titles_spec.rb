a95e628f70e36893ae7c518adbfb9d5c26c8a3c2
fujiyum <91727339+fujiyum@users.noreply.github.com>
2022-02-06 15:35:16 +0000

    [Update]Rspec

--- spec/system/titles_spec.rb
+++ spec/system/titles_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 describe 'タイトル機能', type: :system do
     describe '一覧表示機能' do
       before do
-        user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@com', password: 'password', password_confirmation: 'password')
+        user_a = FactoryBot.create(:user)
         FactoryBot.create(:title, title_name: '最初のタイトル', user: user_a)
       end
-      
-      context 'ユーザーAがログインしているとき' do
+
+      context 'ユーザーがログインしているとき' do
         before do
             visit new_user_session_path
-            fill_in 'メールアドレス', with: 'a@com'
-            fill_in 'パスワード', with: 'password'
-            click_botton 'Log in'
+            fill_in 'user[email]', with: 'test@com'
+            fill_in 'user[password]', with: 'password'
+            click_button 'Log in'
+        end
+
+        it 'ユーザーが作成したタイトルが表示される' do
+            expect(page).to have_content '最初のタイトル'
         end
       end
     end
