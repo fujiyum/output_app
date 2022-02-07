46fee8eb7f2e823004b1d8b2c141241e3debaa58
fujiyum <91727339+fujiyum@users.noreply.github.com>
2022-02-07 12:58:50 +0000

    [Update]Rspec

--- spec/system/titles_spec.rb
+++ spec/system/titles_spec.rb
@@ -2,35 +2,28 @@ require 'rails_helper'
 
 describe 'タイトル機能', type: :system do
     describe '一覧表示機能' do
+      let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@ex.com', password: 'password_A', password_confirmation: 'password_A') }
+      let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@ex.com', password: 'password_B', password_confirmation: 'password_B') }
+      
       before do
-        user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@ex.com', password: 'password_A', password_confirmation: 'password_A')
         FactoryBot.create(:title, title_name: '最初のタイトル', user: user_a)
+        visit new_user_session_path
+        fill_in 'user[email]', with: login_user.email
+        fill_in 'user[password]', with: login_user.password
+        click_button 'Log in'
       end
 
       context 'ユーザーAがログインしているとき' do
-        before do
-            visit new_user_session_path
-            fill_in 'user[email]', with: 'a@ex.com'
-            fill_in 'user[password]', with: 'password_A'
-
-            click_button 'Log in'
-        end
-
+        let(:login_user) {user_a}
+        
         it 'ユーザーが作成したタイトルが表示される' do
             expect(page).to have_content '最初のタイトル'
         end
       end
 
       context 'ユーザーBがログインしているとき' do
-        before do
-          FactoryBot.create(:user, name: 'ユーザーB', email: 'b@ex.com', password: 'password_B', password_confirmation: 'password_B')
-          visit new_user_session_path
-          fill_in 'user[email]', with: 'b@ex.com'
-          fill_in 'user[password]', with: 'password_B'
-
-          click_button 'Log in'
-        end
-
+        let(:login_user) {user_b}
+        
         it 'ユーザーAが作成したタイトルが表示されない' do
           expect(page).to have_no_content '最初のタイトル'
         end
