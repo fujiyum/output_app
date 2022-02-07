ddf78a1ec9eed0880168d1f5f9571855b95ae68f
fujiyum <91727339+fujiyum@users.noreply.github.com>
2022-02-07 12:41:59 +0000

    [Update]Rspec

--- spec/system/titles_spec.rb
+++ spec/system/titles_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 describe 'タイトル機能', type: :system do
     describe '一覧表示機能' do
       before do
-        user_a = FactoryBot.create(:user)
+        user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@ex.com', password: 'password_A', password_confirmation: 'password_A')
         FactoryBot.create(:title, title_name: '最初のタイトル', user: user_a)
       end
 
-      context 'ユーザーがログインしているとき' do
+      context 'ユーザーAがログインしているとき' do
         before do
             visit new_user_session_path
-            fill_in 'user[email]', with: 'test@com'
-            fill_in 'user[password]', with: 'password'
+            fill_in 'user[email]', with: 'a@ex.com'
+            fill_in 'user[password]', with: 'password_A'
+
             click_button 'Log in'
         end
 
@@ -19,5 +20,21 @@ describe 'タイトル機能', type: :system do
             expect(page).to have_content '最初のタイトル'
         end
       end
+
+      context 'ユーザーBがログインしているとき' do
+        before do
+          FactoryBot.create(:user, name: 'ユーザーB', email: 'b@ex.com', password: 'password_B', password_confirmation: 'password_B')
+          visit new_user_session_path
+          fill_in 'user[email]', with: 'b@ex.com'
+          fill_in 'user[password]', with: 'password_B'
+
+          click_button 'Log in'
+        end
+
+        it 'ユーザーAが作成したタイトルが表示されない' do
+          expect(page).to have_no_content '最初のタイトル'
+        end
+      end
+
     end
 end
