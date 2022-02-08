74108cba6eb30ea4eb256a41492e154e9d69bb8f
fujiyum <91727339+fujiyum@users.noreply.github.com>
2022-02-08 13:47:20 +0000

    [Update]Rspec

--- spec/system/titles_spec.rb
+++ spec/system/titles_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe 'タイトル機能', type: :system do
-    describe '一覧表示機能' do
-      let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@ex.com', password: 'password_A', password_confirmation: 'password_A') }
-      let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@ex.com', password: 'password_B', password_confirmation: 'password_B') }
-      
-      before do
-        FactoryBot.create(:title, title_name: '最初のタイトル', user: user_a)
-        visit new_user_session_path
-        fill_in 'user[email]', with: login_user.email
-        fill_in 'user[password]', with: login_user.password
-        click_button 'Log in'
-      end
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@ex.com', password: 'password_A', password_confirmation: 'password_A') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@ex.com', password: 'password_B', password_confirmation: 'password_B') }
+    let!(:title_a) { FactoryBot.create(:title, title_name: '最初のタイトル', user: user_a) }
 
+    before do
+      visit new_user_session_path
+      fill_in 'user[email]', with: login_user.email
+      fill_in 'user[password]', with: login_user.password
+      click_button 'Log in'
+    end
+
+    describe '一覧表示機能' do
       context 'ユーザーAがログインしているとき' do
         let(:login_user) {user_a}
-        
+
         it 'ユーザーが作成したタイトルが表示される' do
             expect(page).to have_content '最初のタイトル'
         end
@@ -23,11 +23,25 @@ describe 'タイトル機能', type: :system do
 
       context 'ユーザーBがログインしているとき' do
         let(:login_user) {user_b}
-        
+
         it 'ユーザーAが作成したタイトルが表示されない' do
           expect(page).to have_no_content '最初のタイトル'
         end
       end
-
     end
+
+    describe '詳細表示機能' do
+      context 'ユーザーAがログインしているとき' do
+        let(:login_user) {user_a}
+
+        before do
+          visit title_path(title_a)
+        end
+
+        it 'ユーザーAが作成したタイトルが表示される' do
+          expect(page).to have_content '最初のタイトル'
+        end
+      end
+    end
+
 end
