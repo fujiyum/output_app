4a8fbb1af88e31213b9a269d412f35872329753e
fujiyum <91727339+fujiyum@users.noreply.github.com>
2022-02-09 14:48:39 +0000

    [Add]introduction_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :profile_image)
+    params.require(:user).permit(:name, :email, :profile_image, :introduction)
   end
 
   def correct_user
